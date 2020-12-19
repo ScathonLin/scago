48af5a8a947b7e63f3dacd5391d544ed62de0080
scathon <scathonlin@126.com>
2020-12-19 23:19:02 +0800

    [commit msg] 切片数据streaming函数式编程方式api [author] linhuadong(scathon) [time] 2020-12-19 23:18

---
+++ collections/stream/stream.go
@@ -0,0 +1,118 @@
+package stream
+
+// description: streaming process api with funcitonal programming like.
+// author: linhuadong.
+// create: 2020-12-19.
+
+import "sort"
+
+type Function func(e interface{}) interface{}
+type Consumer func(e interface{})
+type Supply func() interface{}
+type Predicate func(e interface{}) bool
+type BiConsumer func(e1, e2 interface{}) interface{}
+type Compare func(e1, e2 interface{}) int
+
+type stream struct {
+	head       *stream
+	next       *stream
+	dataset    []interface{}
+	actionFunc func()
+}
+
+//Foreach visits each element in the slice.
+func (strm *stream) Foreach(consumerFunc Consumer) {
+	strm.action()
+	for _, elem := range strm.head.dataset {
+		consumerFunc(elem)
+	}
+}
+
+//Map element to a new elment which be transfered by mapFunc.
+func (strm *stream) Map(mapFunc Function) *stream {
+	nextStream := new(stream)
+	strm.next = nextStream
+	nextStream.head = strm.head
+	nextStream.actionFunc = func() {
+		ds := strm.head.dataset
+		newDataSet := make([]interface{}, len(ds), cap(ds))
+		for i, elem := range ds {
+			newDataSet[i] = mapFunc(elem)
+		}
+		strm.head.dataset = newDataSet
+	}
+	return nextStream
+}
+
+//Filter elements those match the filter condition
+func (strm *stream) Filter(fn Predicate) *stream {
+	nextStream := new(stream)
+	strm.next = nextStream
+	nextStream.head = strm.head
+	nextStream.actionFunc = func() {
+		ds := strm.head.dataset
+		back := make([]interface{}, len(ds), cap(ds))
+		copy(back, ds)
+		cursor := 0
+		for _, elem := range back {
+			if fn(elem) {
+				back[cursor] = elem
+				cursor++
+			}
+		}
+		back = back[:cursor]
+		for i := cursor; i < len(back); i++ {
+			back[i] = nil
+		}
+		strm.head.dataset = back
+	}
+	return nextStream
+}
+
+//Collect all elements after streaming processing.
+func (strm *stream) Collect() []interface{} {
+	strm.action()
+	return strm.head.dataset
+}
+
+//Count the elements after streaming processing.
+func (strm *stream) Count() int {
+	strm.action()
+	return len(strm.head.dataset)
+}
+
+//Distinct the elements after streaming processing.
+func (strm *stream) Distinct(compare Compare) []interface{} {
+	strm.action()
+	ds := strm.head.dataset
+	res := make([]interface{}, len(ds), cap(ds))
+	copy(res, ds)
+	sort.Slice(res, func(i, j int) bool {
+		return compare(res[i], res[j]) < 0
+	})
+	cursor := 0
+	for i, e := range res {
+		if i == 0 || compare(e, res[i-1]) != 0 {
+			res[cursor] = e
+			cursor++
+		}
+	}
+	return res[:cursor]
+}
+
+// action trigger the actual data processing.
+func (strm *stream) action() {
+	processNode := strm.head.next
+	for processNode != nil {
+		processNode.actionFunc()
+		processNode = processNode.next
+	}
+}
+
+//NewStream create a stream handle with given dataset.
+func NewStream(dataset []interface{}) *stream {
+	strm := new(stream)
+	strm.dataset = dataset
+	strm.head = strm
+	return strm
+}
---
+++ collections/stream/stream_test.go
@@ -0,0 +1,123 @@
+package stream
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestStream_Filter(t *testing.T) {
+	nums := []interface{}{1, 2, 3, 4, 5, 6}
+	stream := NewStream(nums)
+	result := stream.Collect()
+	fmt.Println(result)
+	result = stream.Filter(func(num interface{}) bool {
+		n := num.(int)
+		return n%2 == 0
+	}).Collect()
+	fmt.Println(result)
+}
+
+func TestStream_Foreach(t *testing.T) {
+	nums := []interface{}{1, 2, 3, 4, 5, 6}
+	stream := NewStream(nums)
+	var consumerFunc Consumer = func(e interface{}) {
+		fmt.Printf("%v\n", e)
+	}
+	stream.Foreach(consumerFunc)
+	fmt.Println("=======================")
+	stream.Filter(func(num interface{}) bool {
+		n := num.(int)
+		return n%2 == 0
+	}).Foreach(consumerFunc)
+	fmt.Println("End")
+}
+
+func TestStream_Map(t *testing.T) {
+	nums := []interface{}{1, 2, 3, 4, 5, 6}
+	stream := NewStream(nums)
+	stream.Map(func(e interface{}) interface{} {
+		num := e.(int)
+		return num + 2
+	}).Filter(func(e interface{}) bool {
+		return e.(int)%2 == 0
+	}).Foreach(func(e interface{}) {
+		fmt.Printf("%-3v", e)
+	})
+	fmt.Println()
+}
+func TestStream_Map2(t *testing.T) {
+	nums := []interface{}{"linhuadong", "scathon", "scathonlin", "ScathonLin", "RayHauton"}
+	stream := NewStream(nums)
+	stream.Map(func(e interface{}) interface{} {
+		name := e.(string)
+		return strings.ToUpper(name)
+	}).Filter(func(e interface{}) bool {
+		return strings.Contains(e.(string), "SCATHON")
+	}).Foreach(func(e interface{}) {
+		fmt.Println(e)
+	})
+	fmt.Println()
+}
+
+func TestStream_Map3(t *testing.T) {
+	type user struct {
+		age int
+	}
+	nums := []interface{}{1, 2, 3, 4, 5, 6}
+	stream := NewStream(nums)
+	stream.Map(func(e interface{}) interface{} {
+		return user{e.(int)}
+	}).Filter(func(e interface{}) bool {
+		return e.(user).age%2 == 0
+	}).Foreach(func(e interface{}) {
+		fmt.Println(e)
+	})
+	fmt.Println()
+}
+
+func TestStream_Count(t *testing.T) {
+	type user struct {
+		age int
+	}
+	nums := []interface{}{1, 2, 3, 4, 5, 6}
+	stream := NewStream(nums)
+	cnt := stream.Map(func(e interface{}) interface{} {
+		return user{e.(int)}
+	}).Filter(func(e interface{}) bool {
+		return e.(user).age%2 == 0
+	}).Count()
+	fmt.Println(cnt)
+}
+func TestStream_Count2(t *testing.T) {
+	nums := []interface{}{1, 2, 3, 4, 5, 6}
+	stream := NewStream(nums)
+	cnt := stream.Count()
+	fmt.Println(cnt)
+}
+
+func TestStream_Distinct(t *testing.T) {
+	type user struct {
+		name string
+		age  int
+	}
+	ages := []interface{}{1, 2, 6, 8, 2, 5, 9, 5}
+	names := []string{"A", "B", "D", "C", "B", "I", "H", "G"}
+	i := -1
+	stream := NewStream(ages)
+	res := stream.Map(func(e interface{}) interface{} {
+		i++
+		return user{names[i], e.(int)}
+	}).Distinct(func(e1, e2 interface{}) int {
+		u1 := e1.(user)
+		u2 := e2.(user)
+		nameCompare := strings.Compare(u1.name, u2.name)
+		if nameCompare != 0 {
+			return nameCompare
+		}
+		return u1.age - u2.age
+	})
+	for _, u := range res {
+		fmt.Println(u)
+	}
+}
